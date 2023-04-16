6cc5e0224e2aa02034899aee2a6dedf96ceb8381
Paulo Maldonado <paulomllanos@gmail.com>
2023-04-16 12:38:04 -0400

    arreglos de errores

--- src/components/Paypal.jsx
+++ src/components/Paypal.jsx
@@ -8,6 +8,8 @@ export default function Paypal() {
 
     const {cartTotal, clearItemFromCheckout, cartItems} = useContext(CartContext)
     const {reduceStock} = useContext(ProductContext)
+
+    console.log(cartTotal)
     return (
         <PayPalScriptProvider options={{ "client-id": "AcRRjtwNn9pwZGXIZwq-35G53dbvhwFDwBHi-HxtLUAWbWbbIKvtMHuGy3btEYPaDCHfPK8aLiwAyzTl" }}>
             <PayPalButtons 
@@ -27,11 +29,9 @@ export default function Paypal() {
                     return actions.order.capture().then((details) => {
                         const name = details.payer.name.given_name;
                         alert(`Transaction completed by ${name}`);
-                        console.log(data)
                         reduceStock(cartItems)
                         clearItemFromCheckout()
-                       
-                    });
+                    })
                 }}
             />
         </PayPalScriptProvider>
